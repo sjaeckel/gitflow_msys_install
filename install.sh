7191428994e6828b1be93b15f2030cfd5a61b732
Steffen Jaeckel <steffen.jaeckel@stzedn.de>
2012-06-15 16:15:50 +0200

    download binaries and stuff only if non-existant

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 # create ~/bin folder if not existant
 [ -d ~/bin ] || { mkdir ~/bin || exit 1; }
 
-curl https://raw.github.com/stzedn/gitflow_msys_install/master/getopt.exe > ~/bin/getopt.exe
-curl https://raw.github.com/stzedn/gitflow_msys_install/master/libiconv2.dll > ~/bin/libiconv2.dll
-curl https://raw.github.com/stzedn/gitflow_msys_install/master/libintl3.dll > ~/bin/libintl3.dll
-curl https://raw.github.com/bobthecow/git-flow-completion/master/git-flow-completion.bash > ~/git-flow-completion.bash
+# fetch required binaries and stuff
+[ -f ~/bin/getopt.exe ] ||
+  curl https://raw.github.com/stzedn/gitflow_msys_install/master/getopt.exe > ~/bin/getopt.exe
+[ -f ~/bin/libiconv2.dll ] ||
+  curl https://raw.github.com/stzedn/gitflow_msys_install/master/libiconv2.dll > ~/bin/libiconv2.dll
+[ -f ~/bin/libintl3.dll ] ||
+  curl https://raw.github.com/stzedn/gitflow_msys_install/master/libintl3.dll > ~/bin/libintl3.dll
+[ -f ~/git-flow-completion.bash ] ||
+  curl https://raw.github.com/bobthecow/git-flow-completion/master/git-flow-completion.bash > ~/git-flow-completion.bash
 # add sourcing of git-flow-completion to bashrc
 [[ ! -z $(cat ~/.bashrc | grep "source ~/git-flow-completion.bash") ]] ||
   echo "source ~/git-flow-completion.bash" >> ~/.bashrc
